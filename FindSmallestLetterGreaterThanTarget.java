c891b655abd08f09ce3f33e91226757c52bcb0ae
weseewhy <42625189+weseewhy@users.noreply.github.com>
2018-09-14 13:23:13 -0700

    744. Find Smallest Letter Greater Than Target

---
+++ FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,35 @@
+/*
+https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+Given a list of sorted characters letters containing only lowercase letters, and given a target letter target, 
+find the smallest element in the list that is larger than the given target.
+Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+
+Examples:
+Input:
+letters = ["c", "f", "j"]
+target = "a"
+Output: "c"
+
+Input:
+letters = ["c", "f", "j"]
+target = "c"
+Output: "f"
+
+Input:
+letters = ["c", "f", "j"]
+target = "k"
+Output: "c"
+*/
+
+public class FindSmallestLetterGreaterThanTarget {
+    public char nextGreatestLetter(char[] letters, char target) {
+        for (char c : letters) {
+            if (c > target) {
+                return c;
+            }
+        }
+
+        return letters[0];
+    }
+}
