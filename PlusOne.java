b6f58b218b3cd199fd7a849e9ef64872b08a07c8
weseewhy <42625189+weseewhy@users.noreply.github.com>
2018-09-17 10:12:12 -0700

    66. Plus One

---
+++ PlusOne.java
@@ -0,0 +1,46 @@
+/*
+https://leetcode.com/problems/plus-one/
+
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+
+Example 2:
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+*/
+
+public class PlusOne {
+
+    public int[] plusOne(int[] digits) {
+        int sum;
+        int carry = 1;
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+            sum = digits[i] + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+
+            digits[i] = sum;
+        }
+
+        if (carry == 0) {
+            return digits;
+        }
+
+        int[] out = new int[digits.length + 1];
+        out[0] = carry;
+        for (int i = 1; i < out.length; i++) {
+            out[i] = digits[i - 1];
+        }
+
+        return out;
+    }
+}
