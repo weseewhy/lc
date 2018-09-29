c99d71cd342725feb2ceeeb66bd1e0c9626b4dd6
weseewhy <42625189+weseewhy@users.noreply.github.com>
2018-09-28 23:36:37 -0700

    217. Contains Duplicate

---
+++ 217 - ContainsDuplicate.java
@@ -0,0 +1,36 @@
+/*
+https://leetcode.com/problems/contains-duplicate/
+
+Given an array of integers, find if the array contains any duplicates.
+Your function should return true if any value appears at least twice in the array, 
+and it should return false if every element is distinct.
+
+Example 1:
+Input: [1,2,3,1]
+Output: true
+
+Example 2:
+Input: [1,2,3,4]
+Output: false
+
+Example 3:
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> distinctNumbers = new HashSet<>();
+        for (int i : nums) {
+            if (distinctNumbers.contains(i)) {
+                return true;
+            }
+            distinctNumbers.add(i);
+        }
+
+        return false;
+    }
+}
