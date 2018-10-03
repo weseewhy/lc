68de8da995dbdf63cc2d6762bd6ecedbe72ae1a1
weseewhy <42625189+weseewhy@users.noreply.github.com>
2018-10-02 17:43:56 -0700

    101. Symmetric Tree

---
+++ 101 - SymmetricTree.java
@@ -0,0 +1,69 @@
+/*
+https://leetcode.com/problems/symmetric-tree/
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+*/
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isSymmetricRecursive(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode n1, TreeNode n2) {
+        if (n1 == null && n2 == null) {
+            return true;
+        } else if (n1 == null || n2 == null || n1.val != n2.val) {
+            return false;
+        }
+
+        return isMirror(n1.left, n2.right) && isMirror(n1.right, n2.left);
+    }
+
+    public boolean isSymmetricIterative(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root.left);
+        stack.push(root.right);
+
+        while (!stack.isEmpty()) {
+            TreeNode n1 = stack.pop();
+            TreeNode n2 = stack.pop();
+
+            if (n1 == null && n2 == null) {
+                continue;
+            } else if (n1 == null || n2 == null || n1.val != n2.val) {
+                return false;
+            }
+
+            stack.push(n1.left);
+            stack.push(n2.right);
+            stack.push(n1.right);
+            stack.push(n2.left);
+        }
+
+        return true;
+    }
+}
