69911efd7a0cf4a526cada344c2ee04e3e32618c
weseewhy <42625189+weseewhy@users.noreply.github.com>
2018-10-09 19:59:46 -0700

    575. Distribute Candies

---
+++ 575 - DistributeCandies.java
@@ -0,0 +1,39 @@
+/*
+https://leetcode.com/problems/distribute-candies/
+
+Given an integer array with even length, where different numbers in this array represent 
+different kinds of candies. Each number means one candy of the corresponding kind. 
+You need to distribute these candies equally in number to brother and sister. 
+Return the maximum number of kinds of candies the sister could gain.
+
+Example 1:
+Input: candies = [1,1,2,2,3,3]
+Output: 3
+Explanation:
+There are three different kinds of candies (1, 2 and 3), and two candies for each kind.
+Optimal distribution: The sister has candies [1,2,3] and the brother has candies [1,2,3], too. 
+The sister has three different kinds of candies. 
+
+Example 2:
+Input: candies = [1,1,2,3]
+Output: 2
+Explanation: For example, the sister has candies [2,3] and the brother has candies [1,1]. 
+The sister has two different kinds of candies, the brother has only one kind of candies. 
+*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int distributeCandies(int[] candies) {
+        Set<Integer> types = new HashSet<>();
+        for (int i : candies) {
+            types.add(i);
+            if (types.size() >= candies.length / 2) {
+                return candies.length / 2;
+            }
+        }
+
+        return types.size();
+    }
+}
