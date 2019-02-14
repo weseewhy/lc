4ff2c915ca40b3cc4067e5d427e1e24151d4fb0d
weseewhy <42625189+weseewhy@users.noreply.github.com>
2019-02-13 16:29:35 -0800

    560. Subarray Sum Equals K

---
+++ 560 - SubarraySumEqualsK.java
@@ -0,0 +1,47 @@
+/*
+Given an array of integers and an integer k, find the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+Input: nums = [1,1,1], k = 2
+Output: 2
+
+Example 2:
+Input: nums = [10,2,-2,-20,10], k = -10
+Output: 3  
+Explanation: [10,2,-2,-20], [2,-2,-20,10], [-20,10]
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int subarraySum(int[] nums, int target) {
+        int cnt = 0;
+        int sum = 0;
+
+        // Number of times a prefix sum is encountered
+        Map<Integer, Integer> prefixMap = new HashMap<>();
+
+        // Searching for prefix sum 0 implies current sum == target
+        prefixMap.put(0, 1);
+
+        for (int num : nums) {
+            sum += num;
+            // Increment the occurrences of current sum
+            prefixMap.put(sum, prefixMap.getOrDefault(sum, 0) + 1);
+
+            int desiredPrefixSumToReachTarget = sum - target;
+            if (prefixMap.containsKey(desiredPrefixSumToReachTarget)) {
+                cnt += prefixMap.get(desiredPrefixSumToReachTarget);
+            }
+        }
+
+        return cnt;
+    }
+}
+
+/*
+Hint:
+subarray_sum(i, j) = prefixSum(j) - prefixSum(i)  
+At any index, find if the sum of elements in subarray from any of the previous index == target (we use map for fast lookup)
+*/
