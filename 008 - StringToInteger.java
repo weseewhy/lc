9fcbb8bdcc287ceb9c76b77f1d7190789b5115f3
weseewhy <42625189+weseewhy@users.noreply.github.com>
2019-05-17 16:14:42 -0700

    [M] String to Integer (atoi)

---
+++ 008 - StringToInteger.java
@@ -0,0 +1,80 @@
+/*
+https://leetcode.com/problems/string-to-integer-atoi/
+
+Implement atoi which converts a string to an integer.
+
+The function first discards as many whitespace characters as necessary until the first non-whitespace 
+character is found. Then, starting from this character, takes an optional initial plus or minus sign 
+followed by as many numerical digits as possible, and interprets them as a numerical value.
+
+The string can contain additional characters after those that form the integral number, 
+which are ignored and have no effect on the behavior of this function.
+
+If the first sequence of non-whitespace characters in str is not a valid integral number, 
+or if no such sequence exists because either str is empty or it contains only whitespace characters, 
+no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned.
+
+Note:
+Only the space character ' ' is considered as whitespace character.
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range.
+If the numerical value is out of the range of representable values, INT_MAX or INT_MIN is returned.
+
+Example 1:
+Input: "42"
+Output: 42
+
+Example 2:
+Input: "   -42"
+Output: -42
+Explanation: The first non-whitespace character is '-', which is the minus sign.
+             Then take as many numerical digits as possible, which gets 42.
+
+Example 3:
+Input: "4193 with words"
+Output: 4193
+Explanation: Conversion stops at digit '3' as the next character is not a numerical digit.
+
+Example 4:
+Input: "words and 987"
+Output: 0
+Explanation: The first non-whitespace character is 'w', which is not a numerical 
+             digit or a +/- sign. Therefore no valid conversion could be performed.
+
+Example 5:
+Input: "-91283472332"
+Output: -2147483648
+Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
+*/
+
+class Solution {
+    public int myAtoi(String str) {
+        int out = 0;
+        boolean isNegative = false;
+        boolean isNumberStarted = false;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isDigit(c)) {
+                int digit = c - '0';
+                if (Integer.MAX_VALUE / 10 < out || Integer.MAX_VALUE / 10 == out && Integer.MAX_VALUE % 10 < digit) {
+                    return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                }
+
+                isNumberStarted = true;
+                out = out * 10 + digit;
+            } else if (c == '-' || c == '+') {
+                if (isNumberStarted) break;
+                isNegative = c == '-';
+                isNumberStarted = true;
+            } else if (Character.isSpaceChar(c)) {
+                if (isNumberStarted) break;
+            } else {
+                break;
+            }
+        }
+
+        return isNegative ? -out : out;
+    }
+}
