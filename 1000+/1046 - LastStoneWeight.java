002bc4b98866bc798a13ef1b8e92966323ddad53
weseewhy <42625189+weseewhy@users.noreply.github.com>
2019-05-18 23:35:15 -0700

    [E] Last Stone Weight

---
+++ 1000+/1046 - LastStoneWeight.java
@@ -0,0 +1,45 @@
+/*
+https://leetcode.com/problems/last-stone-weight/
+
+We have a collection of rocks, each rock has a positive integer weight.
+Each turn, we choose the two heaviest rocks and smash them together. 
+Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+If x == y, both stones are totally destroyed;
+If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+
+Example 1:
+Input: [2,7,4,1,8,1]
+Output: 1
+Explanation: 
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+*/
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i : stones) {
+            queue.add(i);
+        }
+
+        while (!queue.isEmpty()) {
+            int big = queue.poll();
+            if (queue.isEmpty()) {
+                return big;
+            }
+
+            int small = queue.poll();
+            if (big != small) {
+                queue.add(big - small);
+            }
+        }
+
+        return 0;
+    }
+}
