2a8ce5b9ad9fb9442f63fb2432316734de5a07f3
weseewhy <42625189+weseewhy@users.noreply.github.com>
2019-05-23 18:41:35 -0700

    [M] Search a 2D Matrix

---
+++ 074 - Search2dMatrix.java
@@ -0,0 +1,54 @@
+/*
+https://leetcode.com/problems/search-a-2d-matrix/
+
+Write an efficient algorithm that searches for a value in an m x n matrix. 
+This matrix has the following properties:
+   - Integers in each row are sorted from left to right.
+   - The first integer of each row is greater than the last integer of the previous row.
+
+Example 1:
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+
+Example 2:
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+Output: false
+*/
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0) return false;
+
+        int left = 0;
+        int right = matrix.length * matrix[0].length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int row = mid / matrix[0].length;
+            int col = mid % matrix[0].length;
+
+            int val = matrix[row][col];
+            if (val == target) {
+                return true;
+            } else if (val < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
