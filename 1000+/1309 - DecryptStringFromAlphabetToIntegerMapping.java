0e51a04995bd978da11c418c9f64fc84a99babbe
weseewhy <42625189+weseewhy@users.noreply.github.com>
2020-04-27 21:27:26 -0700

    [E] Decrypt String from Alphabet to Integer Mapping

---
+++ 1000+/1309 - DecryptStringFromAlphabetToIntegerMapping.java
@@ -0,0 +1,45 @@
+/*
+https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/
+
+Given a string s formed by digits ('0' - '9') and '#'. 
+We want to map s to English lowercase characters as follows:
+   - Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+   - Characters ('j' to 'z') are represented by ('10#' to '26#') respectively. 
+Return the string formed after mapping.
+It's guaranteed that a unique mapping will always exist.
+
+Example 1:
+Input: s = "10#11#12"
+Output: "jkab"
+Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+
+Example 2:
+Input: s = "1326#"
+Output: "acz"
+
+Example 3:
+Input: s = "25#"
+Output: "y"
+
+Example 4:
+Input: s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#"
+Output: "abcdefghijklmnopqrstuvwxyz"
+*/
+
+class Solution {
+    public String freqAlphabets(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int val;
+            if (s.charAt(i) == '#') {
+                val = (s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0');
+                i = i - 2;
+            } else {
+                val = s.charAt(i) - '0';
+            }
+            sb.append((char) ('a' + val - 1));
+        }
+
+        return sb.reverse().toString();
+    }
+}
