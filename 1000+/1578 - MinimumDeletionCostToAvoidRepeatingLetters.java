18436afe576e5f43a8d265331ce3bab34fc15a3c
weseewhy <42625189+weseewhy@users.noreply.github.com>
2020-09-05 21:49:43 -0700

    [M] Minimum Deletion Cost to Avoid Repeating Letters

---
+++ 1000+/1578 - MinimumDeletionCostToAvoidRepeatingLetters.java
@@ -0,0 +1,49 @@
+/*
+https://leetcode.com/problems/minimum-deletion-cost-to-avoid-repeating-letters/
+
+Given a string s and an array of integers cost where cost[i] is the cost of deleting the character i in s.
+Return the minimum cost of deletions such that there are no two identical letters next to each other.
+Notice that you will delete the chosen characters at the same time, in other words, after deleting a character, 
+the costs of deleting other characters will not change.
+
+Example 1:
+Input: s = "abaac", cost = [1,2,3,4,5]
+Output: 3
+Explanation: Delete the letter "a" with cost 3 to get "abac" 
+(String without two identical letters next to each other).
+
+Example 2:
+Input: s = "abc", cost = [1,2,3]
+Output: 0
+Explanation: You don't need to delete any character because there are 
+no identical letters next to each other.
+
+Example 3:
+Input: s = "aabaa", cost = [1,2,3,4,1]
+Output: 2
+Explanation: Delete the first and the last character, getting the string ("aba").
+
+Constraints:
+    . s.length == cost.length
+    . 1 <= s.length, cost.length <= 10^5
+    . 1 <= cost[i] <= 10^4
+    . s contains only lowercase English letters.
+*/
+
+class Solution {
+    public int minCost(String s, int[] cost) {
+        int res = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            int totalCost = cost[i];
+            int maxCost = cost[i];
+            while (i < s.length() - 1 && s.charAt(i + 1) == s.charAt(i)) {
+                totalCost += cost[i + 1];
+                maxCost = Math.max(maxCost, cost[i + 1]);
+                i++;
+            }
+            res += totalCost - maxCost;
+        }
+
+        return res;
+    }
+}
