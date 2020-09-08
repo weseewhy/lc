8996078ed7ebccf9bc31cf1426220d57ea53ebfd
weseewhy <42625189+weseewhy@users.noreply.github.com>
2020-09-08 08:20:01 -0700

    [M] Sequential Digits

---
+++ 1000+/1291 - SequentialDigits.java
@@ -0,0 +1,48 @@
+/*
+https://leetcode.com/problems/sequential-digits/
+
+An integer has sequential digits if and only if each digit in the number 
+is one more than the previous digit. Return a sorted list of all the 
+integers in the range [low, high] inclusive that have sequential digits.
+
+Example 1:
+Input: low = 100, high = 300
+Output: [123,234]
+
+Example 2:
+Input: low = 1000, high = 13000
+Output: [1234,2345,3456,4567,5678,6789,12345]
+
+Constraints: 
+    . 10 <= low <= high <= 10^9
+*/
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= 9; i++) {
+            queue.add(i);
+        }
+
+        List<Integer> out = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            Integer cur = queue.poll();
+            if (cur >= low && cur <= high) {
+                out.add(cur);
+            }
+
+            int lastDigit = cur % 10;
+            if (lastDigit < 9 && cur < high) {
+                // Generate next number. 1234 -> 12345
+                queue.add(cur * 10 + lastDigit + 1);
+            }
+        }
+
+        return out;
+    }
+}
